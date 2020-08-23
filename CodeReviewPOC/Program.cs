5698444e23efcc8fff7adcd8e387769df789c953
PankajArav <pankaj220587@gmail.com>
2020-08-23 14:53:56 +0530

    Add project files.

---
+++ CodeReviewPOC/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CodeReviewPOC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 24;
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
